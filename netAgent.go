3a23b51dbeeab0aeb6636c729a3b394b81a2214d
wizard <wizard_cxy@hotmail.com>
2015-05-08 21:01:19 +0800

    add test and kv store part

--- netAgent.go
+++ netAgent.go
@@ -3,6 +3,9 @@
 package netAgent
 
 import (
+	"bytes"
+	b64 "encoding/base64"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/golang/glog"
@@ -10,7 +13,9 @@ import (
 	_ "github.com/hashicorp/consul/command"
 	"github.com/hashicorp/consul/watch"
 	"github.com/mitchellh/cli"
+	"io/ioutil"
 	"net"
+	"net/http"
 	"os"
 	"time"
 )
@@ -62,7 +67,7 @@ func startConsul(serverMode bool, bootstrap bool, bindAddress string, dataDir st
 	}
 
 	if bootstrap {
-		args = append(args, "-bootstrap-expect 1")
+		args = append(args, "-bootstrap-expect=1")
 	}
 
 	if bindAddress != "" {
@@ -77,6 +82,7 @@ func startConsul(serverMode bool, bootstrap bool, bindAddress string, dataDir st
 	eCh <- ret
 }
 
+// Node operation related
 func Join(addr string) error {
 	ret := Execute("join", addr)
 
@@ -96,6 +102,39 @@ func Leave() error {
 		return errors.New("Error leaving consul cluster")
 	}
 	return nil
+}
+
+const CONSUL_CATALOG_BASE_URL = "http://localhost:8500/v1/catalog/"
+
+type Node struct {
+	Name    string `json:"Name,omitempty"`
+	Address string `json:"Addr,ommitempty"`
+}
+
+func getNodes() ([]Node, error) {
+	url := CONSUL_CATALOG_BASE_URL + "nodes"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		glog.Errorf("Error (%v) get %s", err, url)
+		return nil, errors.New("Get nodes failed")
+	}
+
+	defer resp.Body.Close()
+
+	glog.Infof("Get %s for %s\n", resp.Status, url)
+
+	var nodes []Node
+
+	err = json.NewDecoder(resp.Body).Decode(nodes)
+
+	if err != nil {
+		glog.Errorf("getNodes failed error decoding")
+		return nil, errors.New("Decode error in getNodes")
+	}
+
+	return nodes, nil
+
 }
 
 // Execute function is borrowed from Consul's main.go
@@ -124,6 +163,113 @@ func Execute(args ...string) int {
 	}
 
 	return exitCode
+}
+
+// K/V store
+
+const CONSUL_KV_BASE_URL = "http://localhost:8500/v1/kv/"
+
+type KVRespBody struct {
+	CreateIndex int    `json:"CreateIndex,omitempty"`
+	ModifyIndex int    `json:"ModifyIndex,omitempty"`
+	Key         string `json:"Key,omitempty"`
+	Flags       int    `json:Flags,omitempty"`
+	Value       string `json:Value,omitempty"`
+}
+
+func Get(store string, key string) ([]byte, int, bool) {
+	url := CONSUL_KV_BASE_URL + store + "/" + key
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		glog.Errorf("Error (%v) in Get for %s\n", err, url)
+		return nil, 0, false
+	}
+
+	defer resp.Body.Close()
+
+	glog.Infof("Status of Get %s for %s", resp.Status, url)
+
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+		var jsonBody []KVRespBody
+
+		fmt.Printf("haha %+v %+v", jsonBody, resp.Body)
+
+		body, err := ioutil.ReadAll(resp.Body)
+		err = json.Unmarshal(body, &jsonBody)
+
+		existingValue, err := b64.StdEncoding.DecodeString(jsonBody[0].Value)
+
+		if err != nil {
+			return nil, jsonBody[0].ModifyIndex, false
+		}
+
+		return existingValue, jsonBody[0].ModifyIndex, true
+
+	} else {
+		return nil, 0, false
+	}
+}
+
+const (
+	OK = iota
+	OUTDATED
+	ERROR
+)
+
+// return val indicate the error type
+func Put(store string, key string, value []byte, oldVal []byte) int {
+
+	existingVal, casIndex, ok := Get(store, key)
+
+	if ok && !bytes.Equal(oldVal, existingVal) {
+		return OUTDATED
+	}
+
+	url := fmt.Sprintf("%s%s/%s?cas=%d", CONSUL_KV_BASE_URL, store, key, casIndex)
+	glog.Infof("Updating KV pair for %s %s %s %d", url, key, value, casIndex)
+
+	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(value))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		glog.Errorf("Error creating KV pair for %s", key)
+		return ERROR
+	}
+
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	if string(body) == "false" {
+		return ERROR
+	}
+
+	return OK
+
+}
+
+// return val indicate the error type
+func Delete(store string, key string) int {
+	url := fmt.Sprintf("%s%s/%s", CONSUL_KV_BASE_URL, store, key)
+
+	glog.Infof("Deleting KV pair for %s", url)
+
+	req, err := http.NewRequest("DELETE", url, nil)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		glog.Errorf("Error deleting KV pair %d", key)
+		return ERROR
+	}
+
+	defer resp.Body.Close()
+	return OK
 }
 
 // Watch related
